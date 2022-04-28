f24a50dea69c8de315ffe3e8741ba0604eb6104e
maisahr <ma.isahr7@gmail.com>
2022-04-27 22:07:53 -0700

    Recursividad funciona

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const fs = require('fs');
 const path = require('path');
-const {readDirectory, readAFile, mdToHTML} = require('./utils/readFiles.js');
+const {readDirectory, readAFile} = require('./utils/readFiles.js');
 
 const pathAbsolute = path.resolve(process.argv[2]);
 
-if(fs.lstatSync(pathAbsolute).isDirectory() === true) {
-    readDirectory(pathAbsolute).forEach(file => {
-       readAFile(file);
-    });
-} else {
-    readAFile(pathAbsolute);
-};
+const mdLinks = (pathAbsolute) => {
+    if(fs.lstatSync(pathAbsolute).isDirectory() === true) {
+        readDirectory(pathAbsolute).forEach(file => {
+            mdLinks(file);
+        });
+    } else {
+        readAFile(pathAbsolute);
+    };
+}
+
+mdLinks(pathAbsolute);
