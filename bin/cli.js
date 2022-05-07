2d5eb1dad612c625687913ec2268d31fa01d60da
maisahr <ma.isahr7@gmail.com>
2022-05-07 11:41:16 -0700

    Cambia if del CLI por switch

--- bin/cli.js
+++ bin/cli.js
@@ -1,48 +1,72 @@
 #!/usr/bin/env node
-const yargs = require('yargs');
+const yargs = require('yargs')
+/*   .scriptName("md-links")
+  .usage("Usage: $0 <ruta-del-archivo> [opciones]")
+  .option("v", {
+    alias: "validate",
+    describe: "Valida los enlaces.",
+    demandOption: true,
+    type: "booleano",
+    nargs: 1,
+  })
+  .option("h", {
+    alias: "stats",
+    describe: "The height of the area.",
+    demandOption: "The height is required.",
+    type: "number",
+    nargs: 1,
+  }) */
+
 const {mdLinks} = require('../utils/mdLinks.js');
 
 const options = {validate: true}
 
-if(yargs.argv.validate === true && yargs.argv.stats === true) {
-    mdLinks(process.argv[2], options)
-    .then(result => {
-        const unique = result.filter((link, index, self) => { // self es result
-            return self.findIndex(l => l.href === link.href) === index;
-        });
-        const broken = unique.filter(link => {
-            return link.ok === 'fail';
-        });
-        console.log('Total:', result.length, '\nUnique:', unique.length,'\nBroken:', broken.length);
-    });
+const validation = yargs.argv.validate;
+const stadistics = yargs.argv.stats;
+
+const condition = {
+
 }
 
-else if(yargs.argv.validate === true) {
-    mdLinks(process.argv[2], options)
-    .then(result => {
-        result.forEach(link => {
-            console.log(link.file, link.href, link.ok, link.status, link.text.slice(0, 50));
+switch (yargs.argv.validate || yargs.argv.stats) {
+    case (yargs.argv.validate === true && yargs.argv.stats === true) :
+        mdLinks(process.argv[2], options)
+        .then(result => {
+            const unique = result.filter((link, index, self) => { // self es result
+                return self.findIndex(l => l.href === link.href) === index;
+            });
+            const broken = unique.filter(link => {
+                return link.ok === 'fail';
+            });
+            console.log('Total:', result.length, '\nUnique:', unique.length,'\nBroken:', broken.length);
         });
-    });
-}
+    break;
 
-else if(yargs.argv.stats === true) {
-    options.validate = false;
-    mdLinks(process.argv[2], options)
-    .then(result => {
-         const unique = result.filter((link, index, self) => { // self es result
-            return self.findIndex(l => l.href === link.href) === index;
+    case (yargs.argv.validate === true && yargs.argv.stats !== true) :
+        mdLinks(process.argv[2], options)
+        .then(result => {
+            result.forEach(link => {
+                console.log(link.file, link.href, link.ok, link.status, link.text.slice(0, 50), '\n');
+            });
         });
-        console.log('Total:', result.length, '\nUnique:', unique.length);
-    });
-} 
+    break;
 
-else {
-    options.validate = false;
-    mdLinks(process.argv[2], options)
-    .then(result => {
-        result.forEach(link => {
-            console.log(link.file, link.href, link.text.slice(0, 50));
+    case (yargs.argv.validate !== true && yargs.argv.stats === true) :
+        options.validate = false;
+        mdLinks(process.argv[2], options)
+        .then(result => {
+             const unique = result.filter((link, index, self) => { // self es result
+                return self.findIndex(l => l.href === link.href) === index;
+            });
+            console.log('Total:', result.length, '\nUnique:', unique.length);
         });
-    });
+    break;
+    default:
+        options.validate = false;
+        mdLinks(process.argv[2], options)
+        .then(result => {
+            result.forEach(link => {
+                console.log(link.file, link.href, link.text.slice(0, 50), '\n');
+            });
+        });
 }
